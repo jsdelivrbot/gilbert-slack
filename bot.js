f65653a5f0a22c10c5638cb72106c7943438b672
Glitch (gilbert-slack) <none>
2018-10-16 18:06:38 +0000

    added starter logic for features to be implemented

--- bot.js
+++ bot.js
@@ -1,56 +1,3 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Create a new app via the Slack Developer site:
-
-    -> http://api.slack.com
-
-  Get a Botkit Studio token from Botkit.ai:
-
-    -> https://studio.botkit.ai/
-
-  Run your bot from the command line:
-
-    clientId=<MY SLACK TOKEN> clientSecret=<my client secret> PORT=<3000> studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js
-
-# USE THE BOT:
-
-    Navigate to the built-in login page:
-
-    https://<myhost.com>/login
-
-    This will authenticate you with Slack.
-
-    If successful, your bot will come online and greet you.
-
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
@@ -166,18 +113,28 @@ if (!process.env.clientId || !process.env.clientSecret) {
   }
 }
 
+// Timekeeping Controller & Logic
 controller.on('slash_command',function(bot,message) {
 
-  if (message.text === "help") {
-      bot.replyPrivate(message, "Hello, sir. Try typing 'help' for a more interactive help menu.");
-      return;
+  switch(message.command) {  
+    case "/clockin":
+      bot.replyPrivate(message, "testing clockIN feature");
+      break;
+      
+    case "/clockout":
+      bot.replyPrivate(message, 'test clockOUT feature');
+      break;
+      
+    case "/timereport":
+      bot.replyPrivate(message, 'generate CSV report');
+      break;
   }
-    // reply to slash command
-  if(message.command === '/clockin') {
-        bot.replyPrivate(message,'test clockin function');
+  
+  if (message.text === "help") {
+    bot.replyPrivate(message, "Hello, sir. Try typing 'help' for a more interactive help menu.");
+    return;
   }
 });
-
 
 
 function usage_tip() {
