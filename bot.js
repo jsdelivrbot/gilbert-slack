afa0ec3421e49bbdecadfadaba4e29f5b1a20462
Miguel Gray <graymomentstudios@gmail.com>
2018-10-16 22:34:43 -0400

    installed momentjs

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var env = require('node-env-file');
 env(__dirname + '/.env');
-
+var moment = require('moment');
+moment().format();
 
 if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
   // usage_tip();
@@ -116,148 +117,28 @@ if (!process.env.clientId || !process.env.clientSecret) {
 // Timekeeping Controller & Logic
 controller.on('slash_command',function(bot,message) {
 
-  switch(message.command) {  
+  switch(message.command) {
     case "/clockin":
       bot.replyPrivate(message, "testing clockIN feature");
       break;
-      
+
     case "/clockout":
       bot.replyPrivate(message, 'test clockOUT feature');
       break;
-      
+
     case "/timereport":
       bot.replyPrivate(message, 'generate CSV report');
       break;
   }
-  
+
   if (message.text === "help") {
     bot.replyPrivate(message, "Hello, sir. Try typing 'help' for a more interactive help menu.");
     return;
   }
 });
 
-/*
-
-Botkit Studio Skill module to enhance the "menu" script
-
-*/
-
-
-module.exports = function(controller) {
-    // define a before hook
-    // you may define multiple before hooks. they will run in the order they are defined.
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobefore
-    controller.studio.before('menu', function(convo, next) {
-
-        // do some preparation before the conversation starts...
-        // for example, set variables to be used in the message templates
-        // convo.setVar('foo','bar');
-
-        console.log('BEFORE: menu');
-        // don't forget to call next, or your conversation will never continue.
-        next();
-
-    });
-
-    /* Validators */
-    // Fire a function whenever a variable is set because of user input
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiovalidate
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-    // Validate user input: question_1
-    controller.studio.validate('menu','question_1', function(convo, next) {
-
-        var value = convo.extractResponse('question_1');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: menu VARIABLE: question_1');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_2
-    controller.studio.validate('menu','question_2', function(convo, next) {
-
-        var value = convo.extractResponse('question_2');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: menu VARIABLE: question_2');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_3
-    controller.studio.validate('menu','question_3', function(convo, next) {
-
-        var value = convo.extractResponse('question_3');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: menu VARIABLE: question_3');
-
-        // always call next!
-        next();
-
-    });
-
-    /* Thread Hooks */
-    // Hook functions in-between threads with beforeThread
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobeforethread
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-    // Before the default thread starts, run this:
-    controller.studio.beforeThread('menu','default', function(convo, next) {
-
-        /// do something fun and useful
-        // convo.setVar('name','value');
-
-        console.log('In the script *menu*, about to start the thread *default*');
-
-        // always call next!
-        next();
-    });
-
-    // Before the on_timeout thread starts, run this:
-    controller.studio.beforeThread('menu','on_timeout', function(convo, next) {
-
-        /// do something fun and useful
-        // convo.setVar('name','value');
-
-        console.log('In the script *menu*, about to start the thread *on_timeout*');
-
-        // always call next!
-        next();
-    });
-
-
-    // define an after hook
-    // you may define multiple after hooks. they will run in the order they are defined.
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudioafter
-    controller.studio.after('menu', function(convo, next) {
-
-        console.log('AFTER: menu');
-
-        // handle the outcome of the convo
-        if (convo.successful()) {
-
-            var responses = convo.extractResponses();
-            // do something with the responses
-
-        }
-
-        // don't forget to call next, or your conversation will never properly complete.
-        next();
-    });
-}
+var now = moment();
+Console.log('now');
 
 
 
