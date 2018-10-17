9d334c6e011a9e0d912d1545684f05b5e3c4f7cf
Miguel Gray <graymomentstudios@gmail.com>
2018-10-17 19:42:13 -0400

    separated timeclock skill into skills folder

--- bot.js
+++ bot.js
@@ -122,9 +122,9 @@ if (!process.env.clientId || !process.env.clientSecret) {
 
 // Timekeeping Controller & Logic
 controller.on('slash_command',function(bot,message) {
-  
+
   var clockedIn = new Boolean(0);
-  
+
   switch(message.command) {
     case "/clockin":
         var now = moment().tz("America/New_York").format('LT');
@@ -194,9 +194,9 @@ controller.on('slash_command',function(bot,message) {
 
 //     // check message.actions and message.callback_id to see what action to take...
 //     var now = moment().tz("America/New_York").format('LT');
-  
+
 //     bot.replyInteractive(message, {
-        
+
 //         text: 'Oh! Kind person, I have written down your time to start at: ' + now + '.',
 //         attachments: [
 //             {
@@ -229,71 +229,3 @@ controller.on('slash_command',function(bot,message) {
 //     });
 
 // });
-
-
-controller.hears('menu', 'direct_message', function(bot, message) {
-
-   bot.startConversation(message, function(err, convo) {
-
-    convo.ask({
-        "attachments": [
-                            {
-                                "color": "#8cd5f0",
-                                "callback_id": "timekeeping",
-                                "actions": [
-                                    {
-                                        "style": "primary",
-                                        "type": "button",
-                                        "value": "clockIn",
-                                        "name": "clockIn",
-                                        "text": "Clock In"
-                                    },
-                                    {
-                                        "style": "default",
-                                        "type": "button",
-                                        "value": "generateReport",
-                                        "name": "generateReport",
-                                        "text": "Generate Report"
-                                    },
-                                    {
-                                        "style": "danger",
-                                        "type": "button",
-                                        "value": "clockOut",
-                                        "name": "clockOut",
-                                        "text": "Clock Out"
-                                    }
-                                ],
-                                "fields": [],
-                                "text": "Select an option",
-                                "title": "What would you like to do?",
-                                "footer": "v0.1",
-                                "fallback": "",
-                                "title_link": ""
-                            }
-                        ]
-    },[
-        {
-            pattern: "clockIn",
-            callback: function(reply, convo) {
-                var now = moment().tz("America/New_York").format('LT');
-                convo.say('Oh! Kind person, I have written down your time to start at: ' + now + '.');
-                convo.next();
-
-            }
-        },
-        {
-            pattern: "clockOut",
-            callback: function(reply, convo) {
-                convo.say('Too bad');
-                convo.next();
-            }
-        },
-        {
-            default: true,
-            callback: function(reply, convo) {
-                // do nothing
-            }
-        }
-    ]);
-});
-});
--- skills/main_menu.js
+++ skills/main_menu.js
@@ -1,122 +1,66 @@
-/*
+controller.hears('menu', 'direct_message', function(bot, message) {
 
-Botkit Studio Skill module to enhance the "menu" script
+   bot.startConversation(message, function(err, convo) {
 
-*/
+    convo.ask({
+        "attachments": [
+                            {
+                                "color": "#8cd5f0",
+                                "callback_id": "timekeeping",
+                                "actions": [
+                                    {
+                                        "style": "primary",
+                                        "type": "button",
+                                        "value": "clockIn",
+                                        "name": "clockIn",
+                                        "text": "Clock In"
+                                    },
+                                    {
+                                        "style": "default",
+                                        "type": "button",
+                                        "value": "generateReport",
+                                        "name": "generateReport",
+                                        "text": "Generate Report"
+                                    },
+                                    {
+                                        "style": "danger",
+                                        "type": "button",
+                                        "value": "clockOut",
+                                        "name": "clockOut",
+                                        "text": "Clock Out"
+                                    }
+                                ],
+                                "fields": [],
+                                "text": "Select an option",
+                                "title": "What would you like to do?",
+                                "footer": "v0.1",
+                                "fallback": "",
+                                "title_link": ""
+                            }
+                        ]
+    },[
+        {
+            pattern: "clockIn",
+            callback: function(reply, convo) {
+                var now = moment().tz("America/New_York").format('LT');
+                convo.say('Oh! Kind person, I have written down your time to start at: ' + now + '.');
+                convo.next();
 
-
-module.exports = function(controller) {
-    // define a before hook
-    // you may define multiple before hooks. they will run in the order they are defined.
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobefore
-    controller.studio.before('menu', function(convo, next) {
-
-        // do some preparation before the conversation starts...
-        // for example, set variables to be used in the message templates
-        // convo.setVar('foo','bar');
-
-        console.log('BEFORE: menu');
-        // don't forget to call next, or your conversation will never continue.
-        next();
-
-    });
-
-    /* Validators */
-    // Fire a function whenever a variable is set because of user input
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiovalidate
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-    // Validate user input: question_1
-    controller.studio.validate('menu','question_1', function(convo, next) {
-
-        var value = convo.extractResponse('question_1');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: menu VARIABLE: question_1');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_2
-    controller.studio.validate('menu','question_2', function(convo, next) {
-
-        var value = convo.extractResponse('question_2');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: menu VARIABLE: question_2');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_3
-    controller.studio.validate('menu','question_3', function(convo, next) {
-
-        var value = convo.extractResponse('question_3');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: menu VARIABLE: question_3');
-
-        // always call next!
-        next();
-
-    });
-
-    /* Thread Hooks */
-    // Hook functions in-between threads with beforeThread
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobeforethread
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-    // Before the default thread starts, run this:
-    controller.studio.beforeThread('menu','default', function(convo, next) {
-
-        /// do something fun and useful
-        // convo.setVar('name','value');
-
-        console.log('In the script *menu*, about to start the thread *default*');
-
-        // always call next!
-        next();
-    });
-
-    // Before the on_timeout thread starts, run this:
-    controller.studio.beforeThread('menu','on_timeout', function(convo, next) {
-
-        /// do something fun and useful
-        // convo.setVar('name','value');
-
-        console.log('In the script *menu*, about to start the thread *on_timeout*');
-
-        // always call next!
-        next();
-    });
-
-
-    // define an after hook
-    // you may define multiple after hooks. they will run in the order they are defined.
-    // See: https://botkit.ai/docs/readme-studio.html#controllerstudioafter
-    controller.studio.after('menu', function(convo, next) {
-
-        console.log('AFTER: menu');
-
-        // handle the outcome of the convo
-        if (convo.successful()) {
-
-            var responses = convo.extractResponses();
-            // do something with the responses
-
+            }
+        },
+        {
+            pattern: "clockOut",
+            callback: function(reply, convo) {
+                convo.say('Too bad');
+                convo.next();
+            }
+        },
+        {
+            default: true,
+            callback: function(reply, convo) {
+                // do nothing
+            }
         }
-
-        // don't forget to call next, or your conversation will never properly complete.
-        next();
-    });
-}
+    ]);
+});
+});
