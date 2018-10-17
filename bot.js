99355f8249f441cbd8cd8c30b34ac99a84e59824
Glitch (gilbert-slack) <none>
2018-10-17 02:08:38 +0000

    init on ubuntu1

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ env(__dirname + '/.env');
 
 
 if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
-  usage_tip();
+  // usage_tip();
   // process.exit(1);
 }
 
@@ -136,13 +136,137 @@ controller.on('slash_command',function(bot,message) {
   }
 });
 
+/*
 
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
-    console.log('Get Slack app credentials here: https://api.slack.com/apps')
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
+Botkit Studio Skill module to enhance the "menu" script
+
+*/
+
+
+module.exports = function(controller) {
+    // define a before hook
+    // you may define multiple before hooks. they will run in the order they are defined.
+    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobefore
+    controller.studio.before('menu', function(convo, next) {
+
+        // do some preparation before the conversation starts...
+        // for example, set variables to be used in the message templates
+        // convo.setVar('foo','bar');
+
+        console.log('BEFORE: menu');
+        // don't forget to call next, or your conversation will never continue.
+        next();
+
+    });
+
+    /* Validators */
+    // Fire a function whenever a variable is set because of user input
+    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiovalidate
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    // Validate user input: question_1
+    controller.studio.validate('menu','question_1', function(convo, next) {
+
+        var value = convo.extractResponse('question_1');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: menu VARIABLE: question_1');
+
+        // always call next!
+        next();
+
+    });
+
+    // Validate user input: question_2
+    controller.studio.validate('menu','question_2', function(convo, next) {
+
+        var value = convo.extractResponse('question_2');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: menu VARIABLE: question_2');
+
+        // always call next!
+        next();
+
+    });
+
+    // Validate user input: question_3
+    controller.studio.validate('menu','question_3', function(convo, next) {
+
+        var value = convo.extractResponse('question_3');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: menu VARIABLE: question_3');
+
+        // always call next!
+        next();
+
+    });
+
+    /* Thread Hooks */
+    // Hook functions in-between threads with beforeThread
+    // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobeforethread
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    // Before the default thread starts, run this:
+    controller.studio.beforeThread('menu','default', function(convo, next) {
+
+        /// do something fun and useful
+        // convo.setVar('name','value');
+
+        console.log('In the script *menu*, about to start the thread *default*');
+
+        // always call next!
+        next();
+    });
+
+    // Before the on_timeout thread starts, run this:
+    controller.studio.beforeThread('menu','on_timeout', function(convo, next) {
+
+        /// do something fun and useful
+        // convo.setVar('name','value');
+
+        console.log('In the script *menu*, about to start the thread *on_timeout*');
+
+        // always call next!
+        next();
+    });
+
+
+    // define an after hook
+    // you may define multiple after hooks. they will run in the order they are defined.
+    // See: https://botkit.ai/docs/readme-studio.html#controllerstudioafter
+    controller.studio.after('menu', function(convo, next) {
+
+        console.log('AFTER: menu');
+
+        // handle the outcome of the convo
+        if (convo.successful()) {
+
+            var responses = convo.extractResponses();
+            // do something with the responses
+
+        }
+
+        // don't forget to call next, or your conversation will never properly complete.
+        next();
+    });
 }
+
+
+
+// function usage_tip() {
+//     console.log('~~~~~~~~~~');
+//     console.log('Botkit Starter Kit');
+//     console.log('Execute your bot application like this:');
+//     console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
+//     console.log('Get Slack app credentials here: https://api.slack.com/apps')
+//     console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
+//     console.log('~~~~~~~~~~');
+// }
